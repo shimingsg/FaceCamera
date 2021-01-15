719f80e23a88376cf7eefbb20bb14738b3aa857b
shimingsg <v-shige@microsoft.com>
2021-01-15 14:38:31 +0800

    huxiao's work

---
+++ MyApplication/app/src/test/java/com/huxiao/myapplication/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.huxiao.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        System.out.println("1111");
+    }
+}
